308b0b99f722e40b48b6bb116c797b8cd7c6a2d1
AndreiNekrasOn <an-32010@yandex.ru>
2018-11-04 23:07:26 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,22 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+login, password = 'login', 'password'
+vk_session = vk_api.VkApi(login, password)
+
+try:
+    vk_session.auth(token_only=True)
+except vk_api.AuthError as error_msg:
+    print(error_msg)
+
+
+def write_msg(user_id, s):
+    vk_session.method('messages.send', {'user_id': user_id, 'message': s})
+
+
+longpoll = VkLongPoll(vk_session)
+user_id = #integer id
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.from_chat and event.chat_id == 72:
+        write_msg(user_id, event.text)
